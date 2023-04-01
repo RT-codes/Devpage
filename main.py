a8da8fcc67ad2de871022678b150d046f52fe660
RT-codes <rowan.nicolay@gmail.com>
2023-04-01 19:58:44 +0200

    Deployed first commit

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#link variable for url_for
+
+
+
+
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template( 'home.html' )
 
 if __name__ == '__main__':
-  app.run(port=5000)
+    app.run(port=5000)
